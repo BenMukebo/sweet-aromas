51b16e1b1875128eac186647d98f49e8ee2ab2d1
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-10-01 13:29:01 +0200

    Update footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { IconContext } from 'react-icons';
 import { HiPlus } from 'react-icons/hi';
 import { FaGithub, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
-// import { GrEmoji } from 'react-icons/gr';
+import { GrEmoji } from 'react-icons/gr';
 
 const Footer = () => (
   <footer className="d-flex flex-center flex-between footer-main-container">
@@ -18,21 +18,20 @@ const Footer = () => (
         &copy;
         {new Date().getFullYear()}
         , Built with  ❤️
-        {' '}
       </span>
       <p className="d-flex">
-        <a href="https://barackm.me" target="_blank" rel="noreferrer">
-          @Barack_Mukelenga,
-          {' '}
+        <a href="https://barackm.me" target="_blank" rel="noreferrer" style={{ marginLeft: '5px', marginRight: '5px' }}>
+          @Barack_Mukelenga
         </a>
-        <a href="https://benmukebo.me" target="_blank" rel="noreferrer">
+        &
+        <a href="https://benmukebo.me" target="_blank" rel="noreferrer" style={{ marginLeft: '5px' }}>
           {' '}
           @Ben_Mukebo
         </a>
       </p>
     </div>
     <ul className="d-flex footer-links-wrapper footer-links">
-      <li>
+      {/* <li>
         <a href="https://github.com/barackm" target="_blank" rel="noreferrer">
           <IconContext.Provider value={{ className: 'footer-github-icon' }}>
             <FaGithub />
@@ -52,15 +51,7 @@ const Footer = () => (
             <FaTwitterSquare />
           </IconContext.Provider>
         </a>
-      </li>
-      <li>
-        <a href="https://github.com/barackm" target="_blank" rel="noreferrer">
-          {/* <IconContext.Provider value={{ className: 'footer-github-icon' }}>
-            <GrEmoji />
-          </IconContext.Provider> */}
-          |
-        </a>
-      </li>
+      </li> */}
       <li>
         <a href="https://github.com/benmukebo" target="_blank" rel="noreferrer">
           <IconContext.Provider value={{ className: 'footer-github-icon' }}>
@@ -79,6 +70,13 @@ const Footer = () => (
         <a href="https://twitter.com/BenMukebo" target="_blank" rel="noreferrer">
           <IconContext.Provider value={{ className: 'footer-github-icon' }}>
             <FaTwitterSquare />
+          </IconContext.Provider>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/BenMukebo" target="_blank" rel="noreferrer">
+          <IconContext.Provider value={{ className: 'footer-github-icon' }}>
+            <GrEmoji />
           </IconContext.Provider>
         </a>
       </li>
